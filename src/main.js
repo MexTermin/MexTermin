50a7573f77cf27daffefa97c0fd4989a5df022f4
MexTermin <astronboil.p@hotmail.com>
2021-11-11 11:01:46 -0400

    otra prueba

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import { removeLoadingWhenloaded } from "./scripts/loadingEventHandler";
-import { addToWindow } from "./scripts/vhScrool";
+import { removeLoadingWhenloaded } from "../src/scripts/loadingEventHandler";
+import { addToWindow } from "../src/scripts/vhScrool";
 
 
 createApp(App).mount("#app");
