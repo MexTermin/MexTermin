89cb446e02d3d7d57505c055d6027fb5fab51b09
MexTermin <astronboil.p@hotmail.com>
2021-11-10 22:30:40 -0400

    fixed build

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import "../src/scripts/vhScrool.js";
-import { removeLoadingWhenloaded } from "./scripts/loading";
+import "./scripts/vhScrool.js";
+import { removeLoadingWhenloaded } from "./scripts/loading.js";
 
 
 createApp(App).mount("#app");
