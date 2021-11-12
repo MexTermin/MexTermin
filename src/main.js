225f621f32c74930bbf72238b3c60c15b993621f
MexTermin <astronboil.p@hotmail.com>
2021-11-11 21:01:18 -0400

    chore: remove package lock

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import { removeLoadingWhenloaded } from "./scripts/loadingEventHandler";
-import { addToWindow } from "../src/scripts/vhScrool";
+import { removeLoadingWhenloaded } from "./scripts/loadingEventHandler.js";
+import { addToWindow } from "./scripts/vhScrool.js";
 
 
 createApp(App).mount("#app");
--- vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/src'
-    : '/',
-    entry: {
-        app: 'path/to/main.js'
-    },
-  };
