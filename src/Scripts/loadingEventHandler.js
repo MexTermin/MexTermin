1d6c64604284caa2fae50cf61780bda96ec39a4a
MexTermin <astronboil.p@hotmail.com>
2021-12-05 18:03:34 -0400

    test

--- src/Scripts/loadingEventHandler.js
+++ src/Scripts/loadingEventHandler.js
@@ -1,16 +1,23 @@
 /**
  * Add a event that hidden the loadding sniper when the document content is loaded
  */
- export function removeLoadingWhenloaded() {
-    window.addEventListener("DOMContentLoaded", () => {
-      document.querySelector(".charging").style.display = "none";
-    });
-  }
-  /**
-   * Show the loading sniper
-   */
-  export function showLoading() {
-    window.addEventListener("DOMContentLoaded", () => {
-      document.querySelector(".charging").style.display = "flex";
-    });
-  }
+function removeLoadingWhenloaded() {
+  window.addEventListener("DOMContentLoaded", () => {
+    document.querySelector(".charging").style.display = "none";
+  });
+}
+
+/**
+ * Show the loading sniper
+ */
+function showLoading() {
+  window.addEventListener("DOMContentLoaded", () => {
+    document.querySelector(".charging").style.display = "flex";
+  });
+}
+
+export {
+  showLoading,
+  removeLoadingWhenloaded
+
+}
